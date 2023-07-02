2549885346cbc350f91bfdfd444afaf9b20516d7
hemant <hemantkumar9839341321@gmail.com>
2023-07-02 16:45:10 +0530

    new code

--- users/models.py
+++ users/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# from django.contrib.auth import get_user_model
+
+# # Create your models here.
+
+# User = get_user_model()
+
+
+# class User(AbstractUser):
+#     phone_no = models.CharField(max_length=200, unique=True)
+#     otp = models.IntegerField(default=0)
+#     is_Verified = models.BooleanField(blank=False, default=False)
+
+
+class UserAuthTokens(models.Model):
+    user_info = models.ForeignKey(User, on_delete=models.CASCADE)
+    access_token = models.CharField(max_length=200,blank=True, null=True)
+    refresh_token = models.CharField(max_length=200,blank=True, null=True)
+    added_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'user_auth_tokens'
--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
 # Create your views here.
+
+class RegistrationApi(APIView):
+    def post(self,request):
+        pass
+
+
+
+
+class UserLoginApi(APIView):
+    def post(self,request):
+        pass
