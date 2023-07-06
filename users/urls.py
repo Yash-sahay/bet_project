981fef5b093232584ad706a97b6afb1d4f50f9ed
hemant <hemantkumar9839341321@gmail.com>
2023-07-07 00:10:54 +0530

    login api

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,13 @@
 from django import views
 from django.urls import path
 
-from users.views import RegistrationApi
+from users.views import RegistrationApi,UserLoginApi,LogoutView
 
 
 
 urlpatterns = [
     path('RegistrationApi', RegistrationApi.as_view(), name='RegistrationApi'),
+    path('UserLoginApi', UserLoginApi.as_view(), name='UserLoginApi'),
+    path('LogoutView', LogoutView.as_view(), name='LogoutView'),
    
 ]
--- users/views.py
+++ users/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from users.models import UserAuthTokens
-from users.serializers import RegisterSerializer
+from users.serializers import RegisterSerializer,LoginSerializer
 from rest_framework import status, viewsets, mixins
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth import login, logout, authenticate
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, renderer_classes, permission_classes
 # Create your views here.
 
 # class RegistrationApi(APIView):
@@ -42,12 +45,45 @@ class RegistrationApi(APIView):
             first_name = serializer.validated_data['first_name']
             last_name = serializer.validated_data['last_name']
             email = serializer.validated_data['email']
+            if User.objects.get(username=username):
+                return Response({"message":"username must be quinq!"})
 
             User.objects.create(username=username,password=make_password(password),first_name=first_name,last_name=last_name,email=email)
             return Response({"message":"success"})
+        return Response({"message":"somethings went wrong"})
 
 
-
+from rest_framework_simplejwt.tokens import RefreshToken
 class UserLoginApi(APIView):
     def post(self,request):
-        pass
+        data = request.data
+        serializer = LoginSerializer(data=data)
+        if serializer.is_valid():
+            username = serializer.validated_data['username']
+            password = serializer.validated_data['password']
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+                usr_ob=User.objects.filter(username=username)
+                refresh = RefreshToken.for_user(usr_ob[0])
+
+                user_auth_token_obj = UserAuthTokens.objects.filter(
+                user_info=usr_ob[0])
+                if user_auth_token_obj.exists():
+                    user_auth_token_obj.update(
+                        access_token=refresh.access_token, refresh_token=refresh)
+                else:
+                    UserAuthTokens.objects.create(user_info=usr_ob[0], access_token=refresh.access_token,
+                                                      refresh_token=refresh)
+
+                return Response({'status': True, 'message': 'Log In Successful', 'refresh': str(refresh), 'access': str(refresh.access_token),}, status.HTTP_200_OK)
+            else:
+                
+                return Response({'status': False, 'message': 'Invalid User'}, status.HTTP_200_OK)
+        return Response({'status': False, 'message': 'went wrong'}, status.HTTP_200_OK)
+    
+@permission_classes([IsAuthenticated])    
+class LogoutView(APIView) :
+    def get(self,request):
+        logout(request)
+        return Response({'status': True, 'Message': 'Log out successfully'},status.HTTP_200_OK)
