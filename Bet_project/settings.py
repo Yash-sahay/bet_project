20d29a3c70849561c7cb425ec38dd0c9abf175ff
Yash-sahay <85809708+Yash-sahay@users.noreply.github.com>
2023-07-09 18:26:36 +0530

    Update settings.py
    cross origin access done

--- Bet_project/settings.py
+++ Bet_project/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'users',
     'allusermaster'
 
@@ -59,6 +60,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'Bet_project.urls'
@@ -160,3 +163,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ),
 }
+
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000"
+]
