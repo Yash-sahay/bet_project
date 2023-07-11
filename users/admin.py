267ede92f09b94663db759d3a907673cf10017e2
hemant <hemantkumar9839341321@gmail.com>
2023-07-12 00:12:55 +0530

    new cd

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import UserProfile,UserBalanceInfo,UserTransactionHistory
+
+#Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(UserBalanceInfo)
+admin.site.register(UserTransactionHistory)
+
--- users/models.py
+++ users/models.py
@@ -14,6 +14,18 @@ from django.contrib.auth.models import User
 #     otp = models.IntegerField(default=0)
 #     is_Verified = models.BooleanField(blank=False, default=False)
 
+class UserProfile(models.Model):
+	user_info = models.OneToOneField(User,on_delete = models.CASCADE)
+	mob=models.CharField(max_length=15,blank=True,null=True)
+	created_by=models.ForeignKey(User,on_delete=models.CASCADE, related_name = 'user_profile')
+	added_on = models.DateTimeField(auto_now_add = True)
+	updated_on = models.DateTimeField(auto_now = True)
+
+	class Meta:
+		db_table = 'user_profile_info'
+
+
+
 
 class UserAuthTokens(models.Model):
     user_info = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -24,3 +36,23 @@ class UserAuthTokens(models.Model):
 
     class Meta:
         db_table = 'user_auth_tokens'
+
+class UserBalanceInfo(models.Model):
+	user_info = models.OneToOneField(User,on_delete = models.CASCADE)
+	current_coins = models.IntegerField(default = 0)
+	added_on = models.DateTimeField(auto_now_add = True)
+	updated_on = models.DateTimeField(auto_now = True)
+
+	class Meta:
+		db_table = 'user_balance_info'
+                
+class UserTransactionHistory(models.Model):
+	user = models.ForeignKey(User,on_delete = models.CASCADE,related_name = 'user_transactions')
+	credit_coins = models.FloatField(default = 0)
+	debit_coins = models.FloatField(default = 0)
+	net_coins = models.FloatField(default = 0)
+	game_name = models.CharField(max_length = 100,blank = True,null = True)
+	added_on = models.DateTimeField(auto_now_add = True)
+	
+	class Meta:
+		db_table = 'user_transaction_history'
