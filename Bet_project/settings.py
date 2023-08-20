5009fc118f4a168595c5d45c6ea11eb49dc9e198
hemant <hemantkumar9839341321@gmail.com>
2023-08-20 19:44:49 +0530

    new codes

--- Bet_project/settings.py
+++ Bet_project/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    #'corsheaders',
+    'corsheaders',
     'users',
     'allusermaster'
 
@@ -61,7 +61,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #'corsheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 
 ]
 
@@ -86,8 +86,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'Bet_project.wsgi.application'
 
 
-#Database
-#https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
@@ -110,15 +110,7 @@ DATABASES = {
 # }
 
 
-STATICFILES_DIRS = [BASE_DIR / 'static']
 
-STATIC_URL = '/static/'
-
-STATIC_ROOT = BASE_DIR / 'static'
-
-MEDIA_URL = '/media/'
-
-MEDIA_ROOT = BASE_DIR / 'media'
 
 
 
@@ -157,10 +149,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static/"),
-# ]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "/static/"),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
